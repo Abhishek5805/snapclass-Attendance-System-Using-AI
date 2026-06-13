f358aa7d8104852b8080dd397712ed47d0209e5a
demo <abhishekshinde5805@gmail.com>
2026-06-14 00:16:35 +0530

    add all files

--- src/database/db.py
+++ src/database/db.py
@@ -49,19 +49,19 @@ def create_subject(subject_code, name, section, teacher_id):
     return response.data
 
 def get_teacher_subjects(teacher_id):
-    response = supabase.table('subjects').select("*, subject_students(count), attendance_logs(timestamp)").eq("teacher_id", teacher_id).execute()
+    response = supabase.table('subjects').select("*, subject_students(count), attendence_logs(timestamp)").eq("teacher_id", teacher_id).execute()
     subjects = response.data
 
 
     for sub in subjects:
         sub['total_students'] = sub.get("subject_students", [{}])[0].get('count', 0) if sub.get('subject_students') else 0
-        attendance = sub.get('attendance_logs', [])
+        attendance = sub.get('attendence_logs', [])
         unique_sessions = len(set(log['timestamp'] for log in attendance))
         sub['total_classes'] = unique_sessions
 
 
         sub.pop('subject_student', None)
-        sub.pop('attendance_logs', None)
+        sub.pop('attendence_logs', None)
 
     return subjects
 
@@ -84,15 +84,15 @@ def get_student_subjects(student_id):
 
 
 def get_student_attendance(student_id):
-    response = supabase.table('attendance_logs').select('*, subjects(*)').eq('student_id', student_id).execute()
+    response = supabase.table('attendence_logs').select('*, subjects(*)').eq('student_id', student_id).execute()
     return response.data
 
 
 def create_attendance(logs):
-    response = supabase.table('attendance_logs').insert(logs).execute()
+    response = supabase.table('attendence_logs').insert(logs).execute()
     return response.data
 
 def get_attendance_for_teacher(teacher_id):
-    response = supabase.table('attendance_logs').select("*, subjects!inner(*)").eq('subjects.teacher_id', teacher_id).execute()
+    response = supabase.table('attendence_logs').select("*, subjects!inner(*)").eq('subjects.teacher_id', teacher_id).execute()
     return response.data
 
